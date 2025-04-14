4ce0079493e9f95c1c7fce1544959d17595af927
Prisonedknot <84144212+Prisonedknot@users.noreply.github.com>
2025-04-13 23:24:49 -0700

    started implementing customers
    can now converse with test customer

--- Assets/Scripts/DialougeManager.cs
+++ Assets/Scripts/DialougeManager.cs
@@ -7,16 +7,22 @@ public class DialougeManager : MonoBehaviour
 {
     public TMP_Text _tmpProText;
     public string target;
-    Image Speakersprite;
+    [SerializeField] Image Speakersprite;
     string dialouge;
     string speaker;
     public float delay = 0f;
     public float timeBetween = 0.1f;
 
+    [SerializeField]
+    [Range(0f, 10f)]
+    float canvasSpriteRatio = 4;
+
+    public bool dialogueInProgress = false;
+
     // Use this for initialization
     void Start()
     {
-
+        DisableChildren();
     }
 
     private void Update()
@@ -24,12 +30,32 @@ public class DialougeManager : MonoBehaviour
         _tmpProText.text = speaker + ": " + target;
     }
 
-    [ContextMenu("CallDialgue")]
-    public void Dialouge(string speak, Image sprite, string name)
+    public void DisableChildren()
+    {
+        for(int i = 0; i < gameObject.transform.childCount; i++) 
+        {
+            gameObject.transform.GetChild(i).gameObject.SetActive(false);
+        }
+        
+    }
+    public void EnableChildren()
+    {
+        for (int i = 0; i < gameObject.transform.childCount; i++)
+        {
+            gameObject.transform.GetChild(i).gameObject.SetActive(true);
+        }
+    }
+
+    public void Dialouge(string speak, Sprite sprite, string name)
     {
         dialouge = speak;
         speaker = name;
-        Speakersprite = sprite;
+        Vector2 canvasSize = gameObject.GetComponent<RectTransform>().parent.GetComponent<RectTransform>().sizeDelta;
+        Vector2 spriteSize = new Vector2(0,0);
+        spriteSize.x = canvasSize.x / canvasSpriteRatio;
+        spriteSize.y = (spriteSize.x/sprite.texture.width)* sprite.texture.height;
+        Speakersprite.gameObject.GetComponent<RectTransform>().sizeDelta = spriteSize;
+        Speakersprite.sprite = sprite;
         if (_tmpProText != null)
         {
             StartCoroutine("TypeWriterTMP");
@@ -38,16 +64,15 @@ public class DialougeManager : MonoBehaviour
 
     IEnumerator TypeWriterTMP()
     {
+        target = "";
+        dialogueInProgress = true;
         yield return new WaitForSeconds(delay);
 
         foreach (char c in dialouge)
         {
-            if (target.Length > 0)
-            {
-                target = target.Substring(0, target.Length);
-            }
             target += c;
             yield return new WaitForSeconds(timeBetween);
         }
+        dialogueInProgress = false;
     }
 }
